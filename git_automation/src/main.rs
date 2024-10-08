f24d2a56e5bed0872217958911a17a418f1928a8
Kunal Singh Dadhwal <155142500+Kunal-Singh-Dadhwal@users.noreply.github.com>
2024-10-08 22:57:18 +0530

    This was done by the rust code

---
+++ git_automation/src/main.rs
@@ -0,0 +1,51 @@
+use std::io::{self, Write};
+use std::process::{exit, Command};
+use std::thread::panicking;
+
+fn update_commit_push() {
+    let add_command = Command::new("git")
+        .arg("add")
+        .arg("-A")
+        .output()
+        .expect("Failed to do git add");
+
+    if !add_command.status.success() {
+        eprintln!("Error: Failed to add files to repo");
+        exit(1);
+    }
+    print!("Enter your commit message:- ");
+    io::stdout().flush().unwrap();
+
+    let mut commit_msg = String::new();
+    io::stdin().read_line(&mut commit_msg);
+    let commit_command = Command::new("git")
+        .arg("commit")
+        .arg("-m")
+        .arg(commit_msg)
+        .output()
+        .expect("git commit failed");
+
+    if !commit_command.status.success() {
+        eprintln!("Git commit failed");
+        exit(1);
+    }
+
+    let push_command = Command::new("git")
+        .arg("push")
+        .arg("-u")
+        .arg("origin")
+        .arg("main")
+        .output()
+        .expect("Git push failed");
+
+    if !push_command.status.success() {
+        eprintln!("Git push failed");
+        exit(1);
+    }
+
+    println!("Successfully did code add, commit, push to git");
+}
+
+fn main() {
+    update_commit_push();
+}
