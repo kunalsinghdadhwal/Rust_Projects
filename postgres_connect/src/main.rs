dc0b17a00ddfbd4be1908a4ba9bed71f72f0c276
Kunal Singh Dadhwal <155142500+Kunal-Singh-Dadhwal@users.noreply.github.com>
2024-10-08 14:29:53 +0530

    Postgres Ops

--- postgres_connect/src/main.rs
+++ postgres_connect/src/main.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), Error> {
     client.batch_execute(
         "
         CREATE TABLE IF NOT EXISTS author(
-            id SERIAL PRIMARY KEY,
+            id SERIAL PRIMARY KEY ,
             name VARCHAR NOT NULL,
             country VARCHAR NOT NULL
         )
@@ -15,7 +15,7 @@ fn main() -> Result<(), Error> {
     client.batch_execute(
         "
         CREATE TABLE IF NOT EXISTS book(
-            id SERIAL PRIMARY KEY,
+            id SERIAL PRIMARY KEY ,
             title VARCHAR NOT NULL,
             author_id INTEGER NOT NULL REFERENCES author
         )
---
+++ postgres_ops/src/main.rs
@@ -0,0 +1,38 @@
+use postgres::{Client, Error, NoTls};
+use std::collections::HashMap;
+
+struct Author {
+    _id: i32,
+    name: String,
+    country: String,
+}
+
+fn main() -> Result<(), Error> {
+    let mut client = Client::connect("postgres://postgres:postgres@localhost:5432/library", NoTls)?;
+    let mut authors = HashMap::new();
+    authors.insert(String::from("Chinua Achebe"), "Nigeria");
+    authors.insert(String::from("Rabindarnath tagore"), "India");
+    authors.insert(String::from("Anita Nair"), "India");
+
+    for (key, value) in &authors {
+        let author = Author {
+            _id: 0,
+            name: key.to_string(),
+            country: value.to_string(),
+        };
+
+        client.execute(
+            "INSERT INTO author (name, country) VALUES ($1, $2)",
+            &[&author.name, &author.country],
+        )?;
+    }
+    for row in client.query("SELECT id, name, country FROM author", &[])? {
+        let author = Author {
+            _id: row.get(0),
+            name: row.get(1),
+            country: row.get(2),
+        };
+        println!("Author {} coming from {}", author.name, author.country);
+    }
+    Ok(())
+}
